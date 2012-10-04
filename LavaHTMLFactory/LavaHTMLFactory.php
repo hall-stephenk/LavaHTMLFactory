b135c573bc0437742b3dc609accda8a3b6f2ea75
Randy Yates <>
2012-10-04 18:58:17 -0400

    added formatted output

--- LavaHTMLFactory/LavaHTMLFactory.php
+++ LavaHTMLFactory/LavaHTMLFactory.php
@@ -5,8 +5,10 @@ class LavaTag {
 	protected $_attributes;
 	protected $_children;
 	protected $_styles;
+	protected $_selfClosing;
 	
-	public function __construct($name) {
+	public function __construct($name, $selfClosing = false) {
+		$this->_selfClosing = $selfClosing;
 		$this->_name = $name;
 	}
 	
@@ -16,6 +18,10 @@ class LavaTag {
 				$this->_attributes[$key] = $value;
 			}
 		} else { return false; }
+	}
+	
+	public function setSelfClosing(Bool $selfClosing) {
+		$this->_selfClosing = $selfClosing;
 	}
 	
 	public function delAttribute($key) {
@@ -33,44 +39,112 @@ class LavaTag {
 		}
 	}
 	
-	public function addChild($child) {
-		$this->_children[] = $child;
+	public function addChild($child, $index = null) {
+		if(is_null($index)) {
+			$this->_children[] = $child;
+		} else {
+			$this->_children[$index] = $child;
+		}
 		return true;
 	}
 		
-	public function render() {
-		echo "<" . $this->_name;
+	public function setChild($child, $index) {
+		$this->_children[$index] = $child;
+		return true;
+	}
+	
+	public function render($formatOutput = false, $indent = 0) {
+		if($formatOutput) {
+			echo $this->_renderFormatted(0);
+		} else {
+			echo $this->_renderUnformatted();
+		}
+	}
+	
+	protected function _renderFormatted($indentLevel) {
+		$indent = "";
+		for($i = 0; $i < $indentLevel; $i++) {
+			$indent .= "\t";
+		}
+		$render = "$indent<" . $this->_name;
 		if(isset($this->_attributes) && count($this->_attributes) > 0) {
 			foreach($this->_attributes as $key => $value) {
-				echo " " . $key . "='" . $value . "'";
+				$render .= " " . $key . "='" . $value . "'";
 			}
 		}
 		if(isset($this->_styles) && count($this->_styles) > 0) {
-			echo " style='";
+			$render .= " style='";
 			foreach($this->_styles as $key => $value) {
-				echo "$key:$value;";
+				$render .= "$key:$value;";
 			}
-			echo "'";
-				
+			$render .= "'";
 		}
-		echo ">";	
-		if(isset($this->_children) && count($this->_children) > 0) {
-			foreach($this->_children as $child) {
-				if($child instanceOf LavaTag) {
-					$child->render();
-				} else {
-					echo $child;
+		if($this->_selfClosing) { $render .= " />\n"; }
+		else {
+			$render .= ">\n";	
+			if(isset($this->_children) && count($this->_children) > 0) {
+				foreach($this->_children as $child) {
+					if($child instanceOf LavaTag) {
+						$render .= $child->_renderFormatted($indent + 1);
+					} else {
+						$render .= $indent . "\t" . $child . "\n";
+					}
 				}
 			}
+			$render .= "$indent</" . $this->_name . ">\n";
 		}
-		echo "</" . $this->_name . ">";
-		return true;
+		return $render;
+	}
+	
+	protected function _renderUnformatted() {
+		$render = "<" . $this->_name;
+		if(isset($this->_attributes) && count($this->_attributes) > 0) {
+			foreach($this->_attributes as $key => $value) {
+				$render .= " " . $key . "='" . $value . "'";
+			}
+		}
+		if(isset($this->_styles) && count($this->_styles) > 0) {
+			$render .= " style='";
+			foreach($this->_styles as $key => $value) {
+				$render .= "$key:$value;";
+			}
+			$render .= "'";
+		}
+		if($this->_selfClosing) { $render .= " />"; }
+		else {
+			$render .= ">";	
+			if(isset($this->_children) && count($this->_children) > 0) {
+				foreach($this->_children as $child) {
+					if($child instanceOf LavaTag) {
+						$render .= $child->_renderUnformatted();
+					} else {
+						$render .= $child;
+					}
+				}
+			}
+			$render .= "</" . $this->_name . ">";
+		}
+		return $render;
 	}
 }
 
 class LavaTagFactory {
 	public static function create($name) {
-		return new LavaTag($name);
+		if(self::_isSelfClosing($name)) {
+			return new LavaTag($name, TRUE);
+		} else {
+			return new LavaTag($name);
+		}
+	}
+	private static function _isSelfClosing($name) {
+		$selfClosers = array(
+			"br", "hr", "meta", "input", "link", "img"
+		);
+		if(in_array($name, $selfClosers)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
 
--- test.php
+++ test.php
@@ -3,8 +3,10 @@ include('LavaHTMLFactory/LavaHTMLFactory.php');
 
 $html = LavaTagFactory::create('html');
 $head = LavaTagFactory::create('head');
+$title= LavaTagFactory::create('title');
 $body = LavaTagFactory::create('body');
 $h1	= LavaTagFactory::create('h1');
+$hr	= LavaTagFactory::create('hr');
 $p		= LavaTagFactory::create('p');
 // add text to the h1 tag
 $h1->addChild("Title of the Page");
@@ -13,15 +15,17 @@ $h1->addChild("Title of the Page");
 $body->addAttributes(array("class" => "container"));
 $body->addStyles(array("font-family" => "sans-serif"));
 $body->addChild($h1);
-
+$body->addChild($hr);
 // add 4 paragraphs to the body
-$content = array();
 for($i = 1; $i < 6; $i++) {
 	$a = LavaTagFactory::create('p');	
 	$a->addChild("This is paragraph number $i");
 	$a->addStyles(array("background-color" => "#$i$i$i", "color" => "white", "padding" => "5px"));
 	$body->addChild($a);
 }
+
+// Change 2nd paragraph (remember 0 index)
+$body->setChild(LavaTagFactory::create('p')->addChild("This is a replaced Child Paragraph"), 3);
 
 
 // Add head and body to html
